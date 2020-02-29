ee3b5c02f693702a8dc7d74822abe10ff1c90a86
Anthony Smith <anthony@calphorus.com>
2020-02-29 15:39:00 -0800

    Fix: Misspelling of `subprocess`.

--- Bootstrap.py
+++ Bootstrap.py
@@ -117,7 +117,7 @@ class CSCompiler:
         # Print version information if errors occur in build.
         if 0 != result.returncode:
             args    = [ self.compiler, "--version" ]
-            version = subproces.run( args, stdout=subprocPipe,
+            version = subprocess.run( args, stdout=subprocPipe,
                 stderr=subprocPipe )
             stdout = version.stdout.decode( "utf-8" )
             stderr = version.stderr.decode( "utf-8" )
